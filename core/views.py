1f86a9c74010461f0191294b603330d2d20405bf
Damilola boiyelove <daahrmmieboiye@gmail.com>
2019-02-09 11:24:28 +0100

    Completed frontend view for permision request and added logic for accepting and revoking request

--- core/views.py
+++ core/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, JsonResponse, HttpResponse, Http404
 from django.urls import reverse_lazy
+from django.contrib import messages
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import UserAccessRequest
 
 # Create your views here.
-def custom_403(request):
-	render (request, {}, 'core/403.html')
 
 class CanViewContentTest:
 	def dispatch(self, request, *args, **kwargs):
@@ -32,6 +31,22 @@ class DashboardView(LoginRequiredMixin, CanViewContentTest, TemplateView):
 		context['permission_requests'] = UserAccessRequest.objects.all()
 		return context
 
+	def post(self, request, *args, **kwargs):
+		if request.is_ajax():
+			form_name = request.POST.get('form-name')
+			if form_name == 'permission_request_form':
+				user = User.objects.get(id=int(request.POST.get('user_id')))
+				permission_status = request.POST.get('permission_status')
+				useraccessrequest = user.useraccessrequest
+				if permission_status == True:
+					useraccessrequest.is_allowed = True
+					useraccessrequest.save()
+				else:
+					useraccessrequest.is_allowed = False
+					useraccessrequest.save()
+			return HttpResponse('something')
+		raise Http404
+
 class PermissionRequiredView(LoginRequiredMixin, TemplateView):
 	template_name = 'core/permission_required.html'
 
@@ -50,4 +65,3 @@ class PermissionRequiredView(LoginRequiredMixin, TemplateView):
 		context = self.get_context_data(**kwargs)
 		context['access_granted'] = True
 		return self.render_to_response(context)
-
