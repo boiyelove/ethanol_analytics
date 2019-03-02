28cd27527efd34a43c058f2489e59c99f741a7a0
Damilola boiyelove <daahrmmieboiye@gmail.com>
2019-03-02 23:31:46 +0100

    added specific widgets for form fields

--- experiments/forms.py
+++ experiments/forms.py
@@ -5,3 +5,8 @@ class ExperimentForm(forms.ModelForm):
 	class Meta:
 		model = Experiment
 		exclude = ('created', 'updated', 'created_by', 'modified_by')
+		widgets = {
+		'goal': forms.Textarea(attrs={'rows':3}),
+		'start_date': forms.SelectDateWidget(),
+		'end_date': forms.SelectDateWidget(),
+		}
