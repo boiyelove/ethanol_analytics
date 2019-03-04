4b651df2bc25f446055b3a6d47c3c5a7ba61e992
Damilola boiyelove <daahrmmieboiye@gmail.com>
2019-03-04 23:27:10 +0100

    Added models to admin

--- experiments/admin.py
+++ experiments/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Asset, Experiment, Metric, AssetCategory
 # Register your models here.
+
+app_models = [Asset, Experiment, Metric, AssetCategory]
+for model in app_models:admin.site.register(model)
+
