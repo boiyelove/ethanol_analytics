b21cb222ec8c9640a6916339b56b0e3f085250bd
Damilola boiyelove <daahrmmieboiye@gmail.com>
2019-03-11 13:24:15 +0100

    Debugged experiment createtion

--- experiments/views.py
+++ experiments/views.py
@@ -1,19 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView, UpdateView
 from django.views.generic import ListView, DetailView
+from django.contrib.auth.models import User
 from .forms import ExperimentForm
 from .models import Experiment
 
 # Create your views here.
 class CreateExperiment(CreateView):
 	form_class = ExperimentForm
-	success_url = reverse_lazy('list-experiments')
+	success_url = reverse_lazy('experiments:list-experiments')
 	template_name = 'experiments/experiment_form.html'
 
 	def form_valid(self, form):
-		form.author = self.request.user
-		return super().form_valid(form)
+		self.object = form.save(commit=False)
+		self.object.created_by = self.request.user
+		self.object.save()
+		return HttpResponseRedirect(self.success_url)
+
 
 class UpdateExperiment(UpdateView):
 	form_class = ExperimentForm
@@ -21,8 +26,11 @@ class UpdateExperiment(UpdateView):
 	template_name = 'experiments/experiment_form.html'
 
 	def form_valid(self, form):
-		form.author = self.request.user
-		return super().form_valid(form)
+		self.object = form.save(commit=False)
+		self.object.modified_by.add(self.request.user)
+		self.object.save()
+		return HttpResponseRedirect(self.success_url)
+
 
 class ExperimentList(ListView):
 	model = Experiment
