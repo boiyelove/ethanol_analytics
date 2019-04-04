b0c42dd60d2f88a6b107b7387526f3e2803d3754
Damilola Boiyelove <daahrmmieboiye@gmail.com>
2019-04-04 13:20:26 +0100

    updated url routes to suite dataupload-app naming convension

--- datauploads/urls.py
+++ datauploads/urls.py
@@ -3,9 +3,9 @@ from .dashapps import dash_app1, dash_app2, dash_app3
 from . import views
 
 
-app_name ='reports'
+app_name ='datauploads'
 
 urlpatterns =  [
-	path('', views.ReportView.as_view(), name='view-report'),
-	path('<int:id>/', views.ReportView.as_view(), name='view-report_detail'),
+	path('', views.DataUploadView.as_view(), name='view-dataupload'),
+	path('<int:id>/', views.DataUploadView.as_view(), name='view-dataupload_detail'),
 ]
