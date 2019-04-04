9340a0d4387a0df7c1b0caf0eb55d70822aef179
Damilola Boiyelove <daahrmmieboiye@gmail.com>
2019-04-04 13:47:51 +0100

    added dataupload formview

--- datauploads/views.py
+++ datauploads/views.py
@@ -1,6 +1,8 @@
 from django.views.generic import TemplateView
+from django.forms.generic.edit import FormView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import  get_report_data
+from .forms import DataUploadForm, DataFileUploadForm
 
 # Create your views here.
 class DataUploadView(LoginRequiredMixin, TemplateView):
@@ -15,4 +17,13 @@ class DataUploadView(LoginRequiredMixin, TemplateView):
 			self.template_name = "datauploads/datalist.html"
 		else:
 			kwargs['data_list'] = get_report_data()
+		return super().get_context_data(**kwargs)
+
+class DataUploadForm(LoginRequiredMixin, FormView):
+	template_name = 'core/forms.html'
+
+	def get_context_data(self, **kwargs):
+		f_t = kwargs.get('form', 'file')
+		if f_t == 'file':
+			self.form_class = DataFileUploadForm
 		return super().get_context_data(**kwargs)
