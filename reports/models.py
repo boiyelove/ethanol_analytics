71edf23e32a867d5356ece8a1574a22b509824d5
Damilola Boiyelove <daahrmmieboiye@gmail.com>
2019-04-04 09:45:25 +0100

    updated sql query

--- reports/models.py
+++ reports/models.py
@@ -7,11 +7,14 @@ def get_report_data(id=None):
 	
 	with connections['sensors'].cursor() as cursor:
 		if id:
-			cursor.execute("Select * from dash_app_metadata where id = {0}".format(id))
+			cursor.execute("Select * from dash_app_metadata (where category = 'reports') AND (where id = {0})".format(id))
 			result =  cursor.fetchone()
 			columns = [x.name for x in cursor.description]
 			result = dict(zip(columns, result))
 			return result
 		else:
-			cursor.execute("Select * from dash_app_metadata")
+			cursor.execute("Select * from dash_app_metadata where category = 'reports'")
 			return dictfetchall(cursor)
+
+
+
